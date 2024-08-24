3627c96415a12b2268df826a3021a1dcef2b02da
nomankarim8 <karimnoman97@gmail.com>
2024-08-24 07:03:12 -0700

    update Turtle.py

--- Turtle.py
+++ Turtle.py
@@ -1,33 +1,30 @@
-from turtle import *
+from turtle import*
 
 import colorsys
-
 speed(0)
 
 bgcolor("black")
 
-h = 0
+noman = 0
 
 for i in range(16):
 
     for j in range(18):
-
-        c = colorsys.hsv_to_rgb(h, 1, 1)
+        c = colorsys.hsv_to_rgb(noman, h, 1)
 
         color(c)
-
-        h += 0.005
+        noman += 0.005
 
         rt(90)
 
-        circle(150 - j * 6, 90)
+        circle(150 -j * 6, 90)
 
         lt(90)
 
         circle(150 - j * 6, 90)
         
         rt(180)
-        
-    circle(40, 24)
 
-done()
+        circle(40, 24)
+
+        done()
