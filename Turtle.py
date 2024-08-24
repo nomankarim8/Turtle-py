607a233902302ca3a684d2e594b6de21d8af01df
nomankarim8 <karimnoman97@gmail.com>
2024-08-24 07:23:12 -0700

    update Turtle.py

--- Turtle.py
+++ Turtle.py
@@ -6,17 +6,17 @@ speed(0)
 
 bgcolor("black")
 
-h = 0
+NK = 0
 
 for i in range(16):
 
     for j in range(18):
 
-        c = colorsys.hsv_to_rgb(h, 1, 1)
+        c = colorsys.hsv_to_rgb(NK, 1, 1)
 
         color(c)
 
-        h += 0.005
+        NK += 0.005
 
         rt(90)
 
